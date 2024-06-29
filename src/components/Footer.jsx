4d42b70af026bd719ae333fbbe063e3575171ba6
Mathanagopal R <appmechs@gmail.com>
2024-06-29 08:55:15 +0530

    React shopping cart - footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 
 
-
 function Footer() {
     return (
         <> <footer className="py-5 bg-dark">
-        <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Shopping Card 2024</p></div>
+        <div className="container"><p className="m-0 text-center text-white">Copyright &copy; RSC 2024</p></div>
     </footer></>
       )
        
